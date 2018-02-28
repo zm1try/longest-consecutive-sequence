0102de69fab9ed5b6f43ee1b82e28d5a839b9795
zm1try <zmitryarosh@gmail.com>
2018-02-28 12:03:27 +0300

    85/102

--- src/index.js
+++ src/index.js
@@ -1,30 +1,54 @@
 module.exports = function longestConsecutiveLength(array) {
   var iStart;
   var value;
-  var count = 0;
+  var count = 1;
   var maxCount = 1;
+  var arr = array.sort(function(a, b) {
+    return a - b;
+  });
 
-  if (array.length < 1) return 0;
+  if (arr.length < 1) return 0;
 
-  for(let i = 0; i < array.length;) {
-    if(count == 0) {
-      iStart = i;
-      value = array[i];
-      i = 0;
-      count = 1;
-    }
-    if (array[i] - value == count) {
-      count = count + 1;
-      if (maxCount < count) maxCount = count;
-      i = 0;
+  for(let i = 0; i < arr.length; i++) {
+    if (arr[i+1]-arr[i] == 1) {
+      count ++;
+      if (count > maxCount) maxCount = count;
     }
     else {
-      if (i == array.length - 1 && iStart != array.length - 1) {
-        count = 0;
-        i = iStart + 1;
-      }
-      else i = i + 1;
+      count = 1;
     }
   }
   return maxCount;
 }
+
+// module.exports = function longestConsecutiveLength(array) {
+//   var iStart;
+//   var value;
+//   var count = 0;
+//   var maxCount = 1;
+
+//   if (array.length < 1) return 0;
+
+//   for(let i = 0; i < array.length;) {
+//     if(count == 0) {
+//       iStart = i;
+//       value = array[i];
+//       i = 0;
+//       count = 1;
+//     }
+//     if (array[i] - value == count) {
+//       count = count + 1;
+//       if (maxCount < count) maxCount = count;
+//       else i = 0;
+//       continue;
+//     }
+//     else {
+//       if (i == array.length - 1 && iStart != array.length - 1) {
+//         count = 0;
+//         i = iStart + 1;
+//       }
+//       else i = i + 1;
+//     }
+//   }
+//   return maxCount;
+// }
