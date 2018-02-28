1378967cf120dd3dd19ab5f2b2001f74420ee9eb
zm1try <zmitryarosh@gmail.com>
2018-02-28 09:38:55 +0300

    67/102

--- src/index.js
+++ src/index.js
@@ -4,30 +4,27 @@ module.exports = function longestConsecutiveLength(array) {
   var count = 0;
   var maxCount = 1;
 
-  
-
   if (array.length < 1) return 0;
+
   for(let i = 0; i < array.length;) {
-    if(count === 0) {
+    if(count == 0) {
       iStart = i;
       value = array[i];
       i = 0;
       count = 1;
     }
-    if (array[i] - value === count) {
-    
+    if (array[i] - value == count) {
       count = count + 1;
       if (maxCount < count) maxCount = count;
       i = 0;
-      continue;
     }
-    if (i === array.length - 1 && iStart !== array.length - 1) {
-      count = 0;
-      i = iStart + 1;
-      
-      continue;
+    else {
+      if (i == array.length - 1 && iStart != array.length - 1) {
+        count = 0;
+        i = iStart + 1;
+      }
+      else i = i + 1;
     }
-    i = i + 1;
   }
   return maxCount;
 }
